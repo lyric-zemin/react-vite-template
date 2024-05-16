6f9154696ad4ceb4575a96e1bbd3586c3378ff67
lyric <2443835100@qq.com>
2024-05-16 09:27:51 +0800

    chore: 代码优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import mkcert from "vite-plugin-mkcert";
 import tsPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    https: true,
-    port: 5173,
-  },
-  plugins: [
-    react(),
-    mkcert({
-      source: "coding",
-    }),
-    tsPaths(),
-  ],
+  plugins: [react(), tsPaths()],
   build: {
     cssMinify: "lightningcss",
     rollupOptions: {
