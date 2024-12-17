f3240cb722e2dbe5195f58a7c3cc803473689f0f
lyric <2443835100@qq.com>
2024-12-17 11:33:36 +0800

    chore: 代码优化及依赖调整

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import '@unocss/reset/tailwind-compat.css'
 import 'virtual:uno.css'
 
 import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { Spin } from 'antd'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
@@ -10,7 +11,17 @@ import { routeTree } from './routeTree.gen'
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+
+  defaultViewTransition: true,
+  defaultNotFoundComponent: () => <div>404</div>,
+  defaultPendingComponent: () => (
+    <div className="h-100vh flex-center text-5">
+      <Spin />
+    </div>
+  ),
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,33 +1,19 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { ConfigProvider, Spin } from 'antd'
-import { Suspense } from 'react'
+import { ConfigProvider } from 'antd'
 
 export const Route = createRootRoute({
   component: Root,
-  notFoundComponent: () => <Notfound />,
 })
 
 function Root() {
   useTilg()
 
   return (
-    <ConfigProvider>
-      <Suspense
-        fallback={
-          <div className="h-100vh flex-center text-5">
-            <Spin />
-          </div>
-        }
-      >
-        <Outlet />
-      </Suspense>
+    <ConfigProvider theme={{ hashed: false }}>
+      <Outlet />
 
       <TanStackRouterDevtools position="bottom-right" />
     </ConfigProvider>
   )
-}
-
-function Notfound() {
-  return <div>404</div>
 }
