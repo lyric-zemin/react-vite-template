9c003f03268709bb901b30caee5a191f2bb531b9
lyric <2443835100@qq.com>
2025-01-08 16:55:39 +0800

    feat: 增加着色器与模型文件支持

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 import unoCSS from 'unocss/vite'
 import autoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
+import glsl from 'vite-plugin-glsl'
+import gltf from 'vite-plugin-gltf'
 import tsPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
@@ -35,5 +37,11 @@ export default defineConfig({
       ],
       dts: './src/types/auto-imports.d.ts',
     }),
+    // https://github.com/nytimes/rd-bundler-3d-plugins
+    // 模型文件支持
+    gltf(),
+    // https://github.com/UstymUkhman/vite-plugin-glsl
+    // 着色器文件格式支持
+    glsl(),
   ],
 })
